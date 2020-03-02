c75a342ca2968fc87a5bf8d8b72af69cb4c0ef53
yeounseo <yeoandante@gmail.com>
2020-03-02 17:55:57 +0900

    add config files and modified index.js

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
+// 여기서 MONGO_URI는 Heroku에서 가져온다.
--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded 을 분석해서 가져올 수 있게 설정
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 을 분석해서 가져올 수 있게 설정
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://adminUser:qwer1234@youtubeclone-mwkd4.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true,
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.get('/', function (req, res) {
-    res.send('hello node!');
+    res.send('hello nodemon!');
 });
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example  app listening on port ${port}`));
