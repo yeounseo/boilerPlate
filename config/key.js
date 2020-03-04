7b4f0c873b75ed6702d7c9f086e1d85f97cb350f
yeounseo <yeoandante@gmail.com>
2020-03-04 12:36:24 +0900

    modified config files

--- config/key.js
+++ config/key.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
+// process.env <-- 노드의 환경 변수가 production이 되었다면 ?
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
 } else {
-    module.exports = require('./dev');
+  // local 환경 이라면 ?
+  module.exports = require("./dev");
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
-}
-// 여기서 MONGO_URI는 Heroku에서 가져온다.
+  mongoURI: process.env.MONGO_URI
+};
+// deploy 이후에 여기서 MONGO_URI는 Heroku에서 가져온다.
+// production의 약자 prod
